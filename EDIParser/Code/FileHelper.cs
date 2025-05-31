5ccb1e1279b87c71a4a8905b117dccccc42678bc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-05-31 23:44:41 +0500

    File helper implemented.

---
+++ EDIParser/Code/FileHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace EDIParser.Code
+{
+	public static class FileHelper
+    {
+        public static string[] ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                return File.ReadAllLines(path);
+            }
+
+            return new string[] { };
+        }
+    }
+}
