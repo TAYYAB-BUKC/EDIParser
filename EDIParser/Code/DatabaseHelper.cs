72ff2b73fb305b1837f772d9fd7476dee7fdd8c4
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-01 23:21:01 +0500

    Database helper implemented.

---
+++ EDIParser/Code/DatabaseHelper.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace EDIParser.Code
+{
+    public class DatabaseHelper
+    {
+        public static List<EdiMapping> GetMappingsFromDb(string connectionString, int formatType)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string query = @"SELECT * FROM EdiMapping WHERE 1 = 1 AND FormatType = @FormatType";
+                var mappings = connection.Query<EdiMapping>(query, new { FormatType = formatType }).ToList();
+                return mappings;
+            }       
+        }
+    }
+}
