167b5e15095c9f900d07f89a0b4ce23aa08c2ea9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-06-02 22:04:52 +0500

    EdiParser helper initial implementation added.

---
+++ EDIParser/Code/EdiParserHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace EDIParser.Code
+{
+	public class EdiParserHelper
+    {
+        public static List<Dictionary<string, string>> ParseLines(List<string> ediLines, List<EdiMapping> mappings)
+        {
+            var results = new List<Dictionary<string, string>>();
+            foreach (var line in ediLines)
+            {
+                // Read and Parse each line according to the mapping
+            }
+
+            return results;
+        }
+    }
+}
